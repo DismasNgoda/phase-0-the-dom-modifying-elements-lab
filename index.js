f3a46ad6dd58b27b15aa3e5b1d43fbd51194ae6b
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-08-17 13:19:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement('H1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = 'YOUR-NAME is the champion';
